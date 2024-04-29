ed300baa3edfeb537486d841227e680176967349
André Lama <andre.lama@pucp.edu.pe>
2024-04-29 11:39:26 -0500

    pipi

--- 01_traditional/deploy.R
+++ 01_traditional/deploy.R
@@ -1,8 +1,8 @@
-
+setwd("C:/Users/Usuario/Documents/GitHub/shiny_crud/01_traditional")
 
 app_name <- config::get(file = "shiny_app/config.yml")$app_name
 rsconnect::deployApp(
   appDir = "shiny_app",
-  account = "tychobra",
+#  account = "tychobra",
   appName = app_name
 )
